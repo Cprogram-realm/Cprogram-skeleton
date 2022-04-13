2085b0be6ab4f9af8c337786209cc28ca8bf05b3
Weh <87272164+Weh-gh@users.noreply.github.com>
2022-04-13 21:16:23 +0300

    Update basic.c

--- basic.c
+++ basic.c
@@ -3,7 +3,7 @@
 int main() {
   
   // When performing a textual operation with printf, it is mandatory to put a semicolon at the end of the parameters.
-  printf("Hello, C Programming Language.");
+  printf("hello, world");
     
   return 0;
 }
