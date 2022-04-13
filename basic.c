f42b02480010f71558759bd08bc90aec02715888
Weh <87272164+Weh-gh@users.noreply.github.com>
2022-04-13 21:17:17 +0300

    //

--- basic.c
+++ basic.c
@@ -2,7 +2,7 @@
 
 int main() {
   
-  // When performing a textual operation with printf, it is mandatory to put a semicolon at the end of the parameters.
+  // When performing a textual operation with printf, it is mandatory to put a semicolon at the end of the parameters. It means to finish the process.
   printf("hello, world");
     
   return 0;
